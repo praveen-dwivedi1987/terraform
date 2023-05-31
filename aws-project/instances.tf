c6721cd5d5f4e8d06a989ee20a951b8567b03603
praveen <praveen.dwivedi65@gmail.com>
2023-05-31 16:52:23 +0530

    added Role tag to instances

--- aws-project/instances.tf
+++ aws-project/instances.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "jenkins-master" {
   
   tags = {
     Name = "jenkins_master_tf"
-    Type = "jenkins_master"
+    Role = "jenkins_master"
   }
   depends_on = [aws_main_route_table_association.set-master-default-rt-assoc]
 }
@@ -54,7 +54,7 @@ resource "aws_instance" "jenkins-worker-oregon" {
   tags = {
     Name              = join("_", ["jenkins_worker_tf", count.index + 1])
     Master_Private_IP = aws_instance.jenkins-master.private_ip
-    Type = "jenkins_worker"
+    Role = "jenkins_worker"
   }
   depends_on = [aws_main_route_table_association.set-worker-default-rt-assoc]
 }
